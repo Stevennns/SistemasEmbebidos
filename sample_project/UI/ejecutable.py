be363c2b02ce2d631c24c68aa52a3e72a96628d2
Stevennns <stevens.egli13@gmail.com>
2023-07-08 19:14:18 -0400

    Intefaz funcional, falta agregar funcionalidad

--- sample_project/UI/ejecutable.py
+++ sample_project/UI/ejecutable.py
@@ -4,7 +4,7 @@ from PyQt5 import QtCore, Qt, uic, QtWidgets
 from pyqtgraph import QtGui, PlotWidget
 from interfaz import Ui_Dialog
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread,QTimer
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -20,14 +20,12 @@ from PyQt5.QtWidgets import (
 )
 from threading import Thread
 import serial
-import pyqtgraph
 import numpy
 import os.path as path
 import time
 import scipy.io
-#import pygame
 from serialtest import *
-
+from collections import deque
 
 
 class SerialThread(QThread):
@@ -36,25 +34,72 @@ class SerialThread(QThread):
         self.running = True
         self.data = []
 
+        self.acc_x = deque(maxlen=10)
+        self.acc_y = deque(maxlen=10)
+        self.acc_z = deque(maxlen=10)
+
+        self.gyr_x = deque(maxlen=10)
+        self.gyr_y = deque(maxlen=10)
+        self.gyr_z = deque(maxlen=10)
+
+        self.acc_xg = deque(maxlen=10)
+        self.acc_yg = deque(maxlen=10)
+        self.acc_zg = deque(maxlen=10)
+
+        self.tiempo = deque(maxlen=10)
+        self.count_time = 0
+
     def stop(self):
         self.running = False
         send_end_message()
 
     def plot1(self,cont):
         p = cont.plot_1
+        p.clear()
+
         # Crear los datos de tiempo y temperatura
-        tiempo = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        
+        #tiempo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        p.setYRange(-50, 50, padding=0)
+
         data = self.data
         print("uwu: ",data)
         #temperatura = [25, 24, 23, 22, 21, 20, 19, 18, 17, 16]
 
         # Agregar el gráfico de tiempo vs temperatura al objeto plot_1
-        p.plot(tiempo, data, pen='b', symbol='o')
-
+        line1 = p.plot(list(self.tiempo), list(self.acc_x), pen='b', symbol='o')
+        line2 = p.plot(list(self.tiempo), list(self.acc_y), pen='r', symbol='o')
+        line3 = p.plot(list(self.tiempo), list(self.acc_z), pen='g', symbol='o')
+        
         # Configurar el eje x y el eje y con etiquetas
         p.setLabel('left', 'Temperatura (°C)')
         p.setLabel('bottom', 'Tiempo (s)')
+
+    def plot2(self,cont):
+        p2 = cont.plot_2
+        p2.clear()
+        p2.setYRange(-5, 5, padding=0)
+
+        # Agregar el gráfico de tiempo vs temperatura al objeto plot_1
+        line1 = p2.plot(list(self.tiempo), list(self.gyr_x), pen='b', symbol='o')
+        line2 = p2.plot(list(self.tiempo), list(self.gyr_y), pen='r', symbol='o')
+        line3 = p2.plot(list(self.tiempo), list(self.gyr_z), pen='g', symbol='o')
+        
+        # Configurar el eje x y el eje y con etiquetas
+        p2.setLabel('left', 'Temperatura (°C)')
+        p2.setLabel('bottom', 'Tiempo (s)')
+
+    def plot3(self,cont):
+        p3 = cont.plot_3
+        p3.clear()
+        p3.setYRange(-10, 10, padding=0)
+        # Agregar el gráfico de tiempo vs temperatura al objeto plot_1
+        line1 = p3.plot(list(self.tiempo), list(self.acc_xg), pen='b', symbol='o')
+        line2 = p3.plot(list(self.tiempo), list(self.acc_yg), pen='r', symbol='o')
+        line3 = p3.plot(list(self.tiempo), list(self.acc_zg), pen='g', symbol='o')
+        
+        # Configurar el eje x y el eje y con etiquetas
+        p3.setLabel('left', 'Temperatura (°C)')
+        p3.setLabel('bottom', 'Tiempo (s)')
 
     def run(self):
         try:
@@ -63,7 +108,21 @@ class SerialThread(QThread):
             for data in data_generator:
                 print("data for",data)
                 self.data = list(data)
-                self.plot1(data,cont)
+
+                self.acc_x.append(self.data[0])
+                self.acc_y.append(self.data[1])
+                self.acc_z.append(self.data[2])
+
+                self.gyr_x.append(self.data[3])
+                self.gyr_y.append(self.data[4])
+                self.gyr_z.append(self.data[5])
+
+                self.acc_xg.append(self.data[6])
+                self.acc_yg.append(self.data[7])
+                self.acc_zg.append(self.data[8])
+
+                self.tiempo.append(self.count_time)
+                self.count_time += 1
                 print("data=",self.data)
             #print(self.data)
         except serial.SerialException as e:
@@ -78,7 +137,23 @@ class MainWindow():
         self.parent = parent
         self.serialThread = SerialThread()
         #self.serialRead.readSerialStart()
-        
+
+    def plot(self):
+        return self.serialThread.plot1(self.ui)
+
+    def plot_2(self):
+        return self.serialThread.plot2(self.ui)
+
+    def plot_3(self):
+        return self.serialThread.plot3(self.ui)
+    
+    def start_timer1(self):
+        self.timer1 = QTimer()
+        self.timer1.timeout.connect(self.plot)
+        self.timer1.timeout.connect(self.plot_2)
+        self.timer1.timeout.connect(self.plot_3)
+        self.timer1.setInterval(1000)
+        self.timer1.start()
 
     def stopSerialRead(self):
         print("stop")
@@ -92,8 +167,8 @@ class MainWindow():
         self.ui.test_boton_2.clicked.connect(self.stopSerialRead)
 
     def empezarSerialRead(self):
+        self.start_timer1()
         self.serialThread.start()
-        self.serialThread.plot1(self.ui)
 
     def stopSerialRead(self):
         self.serialThread.stop()
